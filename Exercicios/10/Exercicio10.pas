f958df3a9c374cf2afeda4760c8046ffaf52a4bb
Tarcisio A <tarcisioambrosio@gmail.com>
2016-09-07 19:39:35 -0700

    Exercicio 10 finalizado

--- Exercicios/10/Exercicio10.pas
+++ Exercicios/10/Exercicio10.pas
@@ -33,18 +33,19 @@ Type
   
    Procedure cadastrarVeiculos(var vet: vet_veiculos; n: integer);
   var
+  	confirma:char;
     sexo: char;
     i: integer;
 
   begin
   
- 
+    clrscr;
     for i := 1 to n do
       begin
       	write('Veículo ', i,'/',n);
       	gotoxy(1,2);
   			write('Proprietário: ');
-  			gotoxy(33,2);
+  			gotoxy(53,2);
   			write('Combustível: ');
   			
   			gotoxy(1,3);
@@ -52,39 +53,59 @@ Type
   			gotoxy(33,3);
   			write('Cor: ');
   			
-				gotoxy(1,3);
+				gotoxy(1,4);
   			write('Chassi: ');
-  			gotoxy(33,3);
+  			gotoxy(33,4);
   			write('Ano: ');
-  			gotoxy(63,3);
+  			gotoxy(63,4);
   			write('Placa: ');  	
 				
-				gotoxy(14,2);
-				read(vet[i].proprietario);		
-  			
-  			
-  			
-  			readln(i);
-//        write('Proprietário: ');
-//        readln(vet[i].proprietario);
-//        write('Combustível: ');
-//        readln(vet[i].combustivel);
-//        write('Modelo: ');
-//        readln(vet[i].modelo);
-//				write('Cor: ');
-//        readln(vet[i].cor);
-//				write('Chassi: ');
-//        readln(vet[i].chassi);				             
-//				write('Ano: ');
-//        readln(vet[i].ano);   
-//				write('Placa: ');
-//        readln(vet[i].placa);				        
+				gotoxy(15,2);
+				read(vet[i].proprietario);	
+				gotoxy(66,2);
+				read(vet[i].combustivel);	
+				gotoxy(9,3);
+				readln(vet[i].modelo);
+				gotoxy(38,3);
+				readln(vet[i].cor);
+				gotoxy(9,4);
+				readln(vet[i].chassi);
+				gotoxy(38,4);
+				readln(vet[i].ano);
+				gotoxy(70,4);
+				readln(vet[i].placa);
+				
+				write('Confirma o cadastro do veículo com os dados acima? (S) ou (N) -> ');
+				readln(confirma);
+				if((confirma = 'n') or (confirma = 'N')) then
+					i := i -1;        
          clrscr;
       end;
   end;
   
+  procedure imprimirCarrosPorAnoECor(vet:vet_veiculos;n:integer;ano:integer;cor:string);
+  	var 
+		posicao:integer;
+		i:integer;
+  	
+  	begin
+  		posicao :=0;
+  	  writeln('Carros do ano de: ', ano,' e que tenham a cor: ', cor);  	  
+    	for i := 1 to n do
+    		begin
+    		    if((vet[i].ano = ano) and (vet[i].cor = cor)) then
+    		    	begin
+    		    			writeln(posicao,' -> Modelo: ',vet[i].modelo);    		    			
+    		    			posicao := posicao +1;
+    		    	end;
+    		end;
+    		writeln('Um total de: ',posicao,' carros encontrados.');
+  	end;
+  
   
 Begin  
 	verificarQuantidade(n);
-	cadastrarVeiculos(veiculos,n);
+	cadastrarVeiculos(veiculos,n);                        
+	imprimirCarrosPorAnoECor(veiculos, n, 2009, 'azul');
+	
 End.
